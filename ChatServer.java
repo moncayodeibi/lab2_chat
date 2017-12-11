c792a3a3a7b8b54e095d9c7ee2bd9d224641fad5
moncayodeibi <34117357+moncayodeibi@users.noreply.github.com>
2017-12-10 22:31:32 -0500

    Update ChatServer.java

--- ChatServer.java
+++ ChatServer.java
@@ -1,5 +1,3 @@
-package ec.edu.epn.redes.broadcast;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,53 +6,21 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashSet;
 
-/**
- * A multithreaded chat room server.  When a client connects the
- * server requests a screen name by sending the client the
- * text "SUBMITNAME", and keeps requesting a name until
- * a unique one is received.  After a client submits a unique
- * name, the server acknowledges with "NAMEACCEPTED".  Then
- * all messages from that client will be broadcast to all other
- * clients that have submitted a unique screen name.  The
- * broadcast messages are prefixed with "MESSAGE ".
- *
- * Because this is just a teaching example to illustrate a simple
- * chat server, there are a few features that have been left out.
- * Two are very useful and belong in production code:
- *
- *     1. The protocol should be enhanced so that the client can
- *        send clean disconnect messages to the server.
- *
- *     2. The server should do some logging.
- */
 public class ChatServer {
+     private static final int PORT = 9001; //declaramos una variable para defiir el puerto de eschucha
 
-	/**
-     * The port that the server listens on.
-     */
-    private static final int PORT = 9001;
-
-    /**
-     * The set of all names of clients in the chat room.  Maintained
-     * so that we can check that new clients are not registering name
-     * already in use.
-     */
-    private static HashSet<String> names = new HashSet<String>();
+    private static HashSet<String> names = new HashSet<String>(); //instanciamos una array tipo String para colocar el texto escrito
     
-    /**
-     * The set of all the print writers for all the clients.  This
-     * set is kept so we can easily broadcast messages.
-     */
-    private static HashSet<PrintWriter> writers = new HashSet<PrintWriter>();
-    
-    /**
-     * The appplication main method, which just listens on a port and
-     * spawns handler threads.
-     */
+    private static HashSet<PrintWriter> writers = new HashSet<PrintWriter>();//y de igual manera un array para imprimir el texto escrito
+   
     public static void main(String[] args) throws Exception {
-        System.out.println("The chat server is running.");
+        //Especificamos que el servidor está corriendo
+	System.out.println("The chat server is running.");
+	//ponemos en escucha el puerto 9001    
         ServerSocket listener = new ServerSocket(PORT);
-        try {
+        
+	//mintras esto este escuchando mientras este corriendo    
+	try {
             while (true) {
                 new Handler(listener.accept()).start();
             }
@@ -64,48 +30,42 @@ public class ChatServer {
     }
     
     /**
-     * A handler thread class.  Handlers are spawned from the listening
-     * loop and are responsible for a dealing with a single client
-     * and broadcasting its messages.
+     Clase handler
      */
     private static class Handler extends Thread {
-        private String name;
-        private Socket socket;
-        private BufferedReader in;
-        private PrintWriter out;
+	 //declaramos algunas variables que nos van yudar a manejar el chat
+        private String name; //variable del nombre del usuario del chat
+        private Socket socket;//variable socket para la conexión
+        private BufferedReader in; //variable de lectura
+        private PrintWriter out; // variable de impresión en pantalla
 
         /**
-         * Constructs a handler thread, squirreling away the socket.
-         * All the interesting work is done in the run method.
+         Constructor de inicialización del socket
          */
         public Handler(Socket socket) {
             this.socket = socket;
         }
         /**
-         * Services this thread's client by repeatedly requesting a
-         * screen name until a unique one has been submitted, then
-         * acknowledges the name and registers the output stream for
-         * the client in a global set, then repeatedly gets inputs and
-         * broadcasts them.
+        Método para correr la apliacación 
          */
         public void run() {
             try {
 
-                // Create character streams for the socket.
+                // Instanciamos una variable de escritura
                 in = new BufferedReader(new InputStreamReader(
                     socket.getInputStream()));
+		     // Instanciamos una variable de impresión
                 out = new PrintWriter(socket.getOutputStream(), true);
 
-                // Request a name from this client.  Keep requesting until
-                // a name is submitted that is not already used.  Note that
-                // checking for the existence of a name and adding the name
-                // must be done while locking the set of names.
+                // código para recibir el nombre del cliente
                 while (true) {
+		   //imprimios nombre guardado a lo escrito en el cliente al recibor el nombre 	
                     out.println("SUBMITNAME");
                     name = in.readLine();
                     if (name == null) {
                         return;
                     }
+		//sincronizamos nombres cuando se sigan añadiendo clientes a la aplicación
                     synchronized (names) {
                         if (!names.contains(name)) {
                             names.add(name);
@@ -114,9 +74,7 @@ public class ChatServer {
                     }
                 }
 
-                // Now that a successful name has been chosen, add the
-                // socket's print writer to the set of all writers so
-                // this client can receive broadcast messages.
+                // Con los nombres sicronizados y acepatamos inciamos el socket de comunicación
                 out.println("NAMEACCEPTED");
                 writers.add(out);
 
